783969a0452a85f724a8b06074e36e947cf980e1
flnice666 <flnice666@163.com>
2024-01-03 09:41:58 +0800

    home set swiper

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http;
+
+//请求拦截器
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+  	title:'数据加载中...'
+  });
+}
+
+//响应拦截器
+$http.afterRequest  = function (){
+	uni.hideLoading();
+}
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
-		home
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item, index) in swiperList" :key="i">
+				<view class="swiper-item">
+					<image :src="item.image"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +14,61 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: []
 			};
+		},
+		onLoad() {
+			this.getSwiperList();
+		},
+		methods:{
+			async getSwiperList(){
+				// const {data:res} = uni.$http('');
+				const res = {
+					data:{
+						swiperList:[
+							{
+								image: 'https://images.pexels.com/photos/2444403/pexels-photo-2444403.jpeg',
+								name: '山路'
+							},
+							{
+								image: 'https://images.pexels.com/photos/2286895/pexels-photo-2286895.jpeg',
+								name: '湖水'
+							},
+							{
+								image: 'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+								name: '海底'
+							}
+						]
+					},
+					message:{
+						status: 200
+					}
+				}
+				if(res.message.status !== 200){
+					return 
+					return uni.showToast({
+						title: '接口返回失败！',
+						icon:'none'
+					})
+				}
+				this.swiperList = res.data.swiperList;
+			},
 		}
 	}
 </script>
 
 <style lang="scss">
-
+swiper{
+	height: 330rpx;
+	.swiper-item{
+		width: 100%;
+		height: 100%;
+	}
+	image{
+		width: 100%;
+		height: 100%;
+		
+	}
+	
+}
 </style>
